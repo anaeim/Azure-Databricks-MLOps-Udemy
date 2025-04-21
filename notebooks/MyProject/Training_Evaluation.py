1e3a6e66cd8d27d755cab8e92397aa18717d3eca
ali <ali.naeim.gdrive@gmail.com>
2025-04-20 18:13:27 -0600

    fix minor bugs

--- notebooks/MyProject/Training_Evaluation.py
+++ notebooks/MyProject/Training_Evaluation.py
@@ -14,13 +14,11 @@
 # MAGIC ## Requirements
 # MAGIC - A cluster running Databricks Runtime 6.4 ML or above. Note that if your cluster is running Databricks Runtime 6.4 ML, you must upgrade the installed version of MLflow to 1.7.0. You can install this version from PyPI. See ([AWS](https://docs.databricks.com/libraries/cluster-libraries.html#cluster-installed-library)|[Azure](https://docs.microsoft.com/azure/databricks/libraries/cluster-libraries#cluster-installed-library)) for instructions. 
 
-# COMMAND ----------
 
 # MAGIC %md # Machine learning application: Forecasting wind power
 # MAGIC 
 # MAGIC In this notebook, you use the MLflow Model Registry to build a machine learning application that forecasts the daily power output of a [wind farm](https://en.wikipedia.org/wiki/Wind_farm). Wind farm power output depends on weather conditions: generally, more energy is produced at higher wind speeds. Accordingly, the machine learning models used in the notebook predict power output based on weather forecasts with three features: `wind direction`, `wind speed`, and `air temperature`.
 
-# COMMAND ----------
 
 # MAGIC %md *This notebook uses altered data from the [National WIND Toolkit dataset](https://www.nrel.gov/grid/wind-toolkit.html) provided by NREL, which is publicly available and cited as follows:*
 # MAGIC 
@@ -32,17 +30,14 @@
 # MAGIC 
 # MAGIC *King, J., A. Clifton, and B.M. Hodge. 2014. Validation of Power Output for the WIND Toolkit (Technical Report, NREL/TP-5D00-61714). Golden, CO: National Renewable Energy Laboratory.*
 
-# COMMAND ----------
 
 # MAGIC %md ## Load the dataset
 # MAGIC 
 # MAGIC The following cells load a dataset containing weather data and power output information for a wind farm in the United States. The dataset contains `wind direction`, `wind speed`, and `air temperature` features sampled every eight hours (once at `00:00`, once at `08:00`, and once at `16:00`), as well as daily aggregate power output (`power`), over several years.
 
-# COMMAND ----------
 
 # I am changing the training code for a better version
 
-# COMMAND ----------
 
 import pandas as pd
 wind_farm_data = pd.read_csv("https://github.com/dbczumar/model-registry-demo-notebook/raw/master/dataset/windfarm_data.csv", index_col=0)
@@ -73,32 +68,26 @@ def get_weather_and_forecast():
 
   return weather_and_forecast.drop(columns="power"), past_power_output["power"]
 
-# COMMAND ----------
 
 # MAGIC %md Display a sample of the data for reference.
 
-# COMMAND ----------
 
 wind_farm_data["2019-01-01":"2019-01-14"]
 
-# COMMAND ----------
 
 # MAGIC %md # Train a power forecasting model and track it with MLflow
 # MAGIC 
 # MAGIC The following cells train a neural network to predict power output based on the weather features in the dataset. MLflow is used to track the model's hyperparameters, performance metrics, source code, and artifacts.
 
-# COMMAND ----------
 
 # MAGIC %md Define a power forecasting model using TensorFlow Keras.
 
-# COMMAND ----------
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
 
 
-# COMMAND ----------
 
 def train_keras_model(X, y):
   
@@ -110,16 +99,13 @@ def train_keras_model(X, y):
   model.fit(X_train, y_train, epochs=100, batch_size=64, validation_split=.2)
   return model
 
-# COMMAND ----------
 
 # MAGIC %md Train the model and use MLflow to track its parameters, metrics, artifacts, and source code.
 
-# COMMAND ----------
 
 # MAGIC %md
 # MAGIC This code set the mflow to model registery workspace (Run the using databricks CLI in Bash and make sure Databricks cli is installed) - you have to do this for all of your Dev, staging and prod workspaces
 
-# COMMAND ----------
 
 databricks configure --token
 enter host (with worksapce id start with ?O)
@@ -129,7 +115,6 @@ databricks secrets put --scope modelregistery --key modelregistery-token --strin
 databricks secrets put --scope modelregistery --key modelregistery-workspace-id --string-value 8074051404611178
 databricks secrets put --scope modelregistery --key modelregistery-host --string-value https://adb-8074051404611178.18.azuredatabricks.net/
 
-# COMMAND ----------
 
 import mlflow
 
@@ -137,7 +122,6 @@ registry_uri = f'databricks://modelregistery:modelregistery'
 mlflow.set_registry_uri(registry_uri)
 
 
-# COMMAND ----------
 
 import mlflow
 import mlflow.keras
@@ -153,11 +137,9 @@ with mlflow.start_run():
   train_keras_model(X_train, y_train)
   run_id = mlflow.active_run().info.run_id
 
-# COMMAND ----------
 
 run_id
 
-# COMMAND ----------
 
 # MAGIC %md # Register the model with the MLflow Model Registry API
 # MAGIC 
@@ -165,17 +147,14 @@ run_id
 # MAGIC 
 # MAGIC The following cells use the API to register your forecasting model, add rich model descriptions, and perform stage transitions. See the documentation for the UI workflow.
 
-# COMMAND ----------
 
 model_name = "power-forecasting-model" # Replace this with the name of your registered model, if necessary.
 
-# COMMAND ----------
 
 # MAGIC %md ### Create a new registered model using the API
 # MAGIC 
 # MAGIC The following cells use the `mlflow.register_model()` function to create a new registered model whose name begins with the string `power-forecasting-model`. This also creates a new model version (for example, `Version 1` of `power-forecasting-model`).
 
-# COMMAND ----------
 
 import mlflow
 
@@ -185,13 +164,11 @@ model_uri = "runs:/{run_id}/{artifact_path}".format(run_id=run_id, artifact_path
 
 model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
 
-# COMMAND ----------
 
 # MAGIC %md After creating a model version, it may take a short period of time to become ready. Certain operations, such as model stage transitions, require the model to be in the `READY` state. Other operations, such as adding a description or fetching model details, can be performed before the model version is ready (for example, while it is in the `PENDING_REGISTRATION` state).
 # MAGIC 
 # MAGIC The following cell uses the `MlflowClient.get_model_version()` function to wait until the model is ready.
 
-# COMMAND ----------
 
 import time
 from mlflow.tracking.client import MlflowClient
